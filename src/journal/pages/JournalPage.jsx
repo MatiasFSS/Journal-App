3c002fc05a278ac1e9f4a25165bc99572045d6de
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-09 16:42:17 -0300

    Diseño de NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,12 @@
-
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Nostrud do est sunt officia adipisicing velit non proident commodo non esse est irure consequat. Dolor adipisicing dolor amet tempor laborum magna tempor reprehenderit. Sunt ullamco deserunt nulla eiusmod amet proident commodo ad pariatur nulla. Id qui voluptate pariatur ex eu culpa sunt consequat. Exercitation cupidatat quis deserunt laborum ea esse proident eu do irure sit irure pariatur. Anim amet consectetur esse officia et do non pariatur incididunt do ipsum nostrud adipisicing. Pariatur occaecat veniam incididunt fugiat esse consectetur.</Typography>
-      
+      {/* <Typography>Nostrud do est sunt officia adipisicing velit non proident commodo non esse est irure consequat. Dolor adipisicing dolor amet tempor laborum magna tempor reprehenderit. Sunt ullamco deserunt nulla eiusmod amet proident commodo ad pariatur nulla. Id qui voluptate pariatur ex eu culpa sunt consequat. Exercitation cupidatat quis deserunt laborum ea esse proident eu do irure sit irure pariatur. Anim amet consectetur esse officia et do non pariatur incididunt do ipsum nostrud adipisicing. Pariatur occaecat veniam incididunt fugiat esse consectetur.</Typography> */}
+      <NothingSelectedView/>
     </JournalLayout>
   )
 }

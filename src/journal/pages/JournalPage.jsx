720104cd2eeb3b4c2c02f5d78ef385ae4d2d7105
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-09 16:01:55 -0300

    Diseño de JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { MailOutline } from "@mui/icons-material"
+
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>
-        Journal Page
-      </Typography>
-      <MailOutline/>
-    </>
+    <JournalLayout>
+      
+      <Typography>Nostrud do est sunt officia adipisicing velit non proident commodo non esse est irure consequat. Dolor adipisicing dolor amet tempor laborum magna tempor reprehenderit. Sunt ullamco deserunt nulla eiusmod amet proident commodo ad pariatur nulla. Id qui voluptate pariatur ex eu culpa sunt consequat. Exercitation cupidatat quis deserunt laborum ea esse proident eu do irure sit irure pariatur. Anim amet consectetur esse officia et do non pariatur incididunt do ipsum nostrud adipisicing. Pariatur occaecat veniam incididunt fugiat esse consectetur.</Typography>
+      
+    </JournalLayout>
   )
 }

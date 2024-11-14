5035b0db3c8caed816291c088b77caa1d85fde97
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-14 17:01:30 -0300

    Agregar note a Firestore, y cambiar vista al hacer click en agregar nota

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,34 @@
 import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { AddOutlined } from "@mui/icons-material"
+import { useDispatch, useSelector } from "react-redux"
+import { startNewNote } from "../../store/journal"
+import { NoteView } from "../views/NoteView"
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch()
+  const {isSaving, active: notes} = useSelector(state => state.journal)
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote())
+  }
   return (
     <JournalLayout>
       
-      <NothingSelectedView/>
+      {
+        (!!notes)
+        ?<NoteView/>
+        :<NothingSelectedView/> 
+      }
+
+      {/* <NothingSelectedView/> */}
       {/* <NoteView/> */}
 
       <IconButton
+        onClick={onClickNewNote}
+        disabled={isSaving}
         size='large'
         sx={{
           color:'white',
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const journalSlice = createSlice({
     name: 'journal',
     initialState: {
-        isSaving: true,
+        isSaving: false,
         messageSaved: '',
         notes: [],
         active: null,
@@ -18,11 +18,15 @@ export const journalSlice = createSlice({
         // }
     },
     reducers: {
+        savingNewNote: (state) => {
+            state.isSaving = true
+        },
         addNewEmptyNote: (state, action) => {
-
+           state.notes.push(action.payload)
+           state.isSaving = false
         },
         setActiveNote: (state, action) => {
-
+            state.active = action.payload
         },
         setNotes: (state, action) => {
 
@@ -38,4 +42,4 @@ export const journalSlice = createSlice({
         }
     }
 });
-export const { addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById } = journalSlice.actions;
+export const { savingNewNote, addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById } = journalSlice.actions;

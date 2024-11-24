f29d0de4b0668def3e288166ea06eeea767adc4b
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-24 18:35:30 -0300

    Eliminar nota

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,11 +1,11 @@
-import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { DeleteOutline, SaveOutlined, UploadOutlined } from "@mui/icons-material"
 import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useForm } from "../../hooks/useForm"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useMemo, useRef } from "react"
 import { setActiveNote } from "../../store/journal/journalSlice"
-import { startSaveNote, startUploadingFiles } from "../../store/journal/thunks"
+import { startDeletingNote, startSaveNote, startUploadingFiles } from "../../store/journal/thunks"
 import Swal from "sweetalert2"
 import'sweetalert2/dist/sweetalert2.css'
 
@@ -13,11 +13,9 @@ import'sweetalert2/dist/sweetalert2.css'
 export const NoteView = () => {
 
     const dispatch = useDispatch()
-
     const {active: note, messageSaved, isSaving} = useSelector(state => state.journal)
-
     const {body, title, onInputChange, formState, date} = useForm(note)
-
+    
     const dateString = useMemo(() => {
 
         const newDate = new Date(date)
@@ -46,6 +44,10 @@ export const NoteView = () => {
 
         // console.log('Subiendo archivos')
         dispatch(startUploadingFiles(target.files))
+    }
+
+    const onDelete = () => {
+        dispatch(startDeletingNote())
     }
 
     return (
@@ -111,6 +113,17 @@ export const NoteView = () => {
                     value={body}
                     onChange={onInputChange}
                 />
+            </Grid>
+
+            <Grid container justifyContent='end'>
+                <Button
+                    onClick={onDelete}
+                    sx={{mt:2}}
+                    color="error"
+                >
+                    <DeleteOutline/>
+                    BORRAR
+                </Button>
             </Grid>
 
             {/* Galeria de imagenes */}
